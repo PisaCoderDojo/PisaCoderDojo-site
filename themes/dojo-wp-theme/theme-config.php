332deb2d44c6f1266c4e04e4fcf79a89a5919678
lucarin91 <lucarin91@gmail.com>
2017-06-12 18:52:12 +0200

    update mentors

--- themes/dojo-wp-theme/theme-config.php
+++ themes/dojo-wp-theme/theme-config.php
@@ -1,6 +1,7 @@
 <?php
 
-define("EVENTBRITE_URL", "https://");
-define("EVENTBRITE_DATE", "06-21-2017");
+define("EVENTBRITE_URL", "https://zen.coderdojo.com/dojo/9592081b-bc35-4cce-9794-013d7348587a/event/8d43f6b4-2fea-448d-a4ba-9ab1151e6080");
+// date format: YYYY-MM-DD
+define("EVENTBRITE_DATE", "2017-06-21");
 
 ?>
