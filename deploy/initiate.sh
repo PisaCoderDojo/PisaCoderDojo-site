11e81981a3d3bf67647a15962225e65629cde367
lucarin91 <lucarin91@gmail.com>
2018-11-03 22:17:14 +0100

    update deployment script

---
+++ deploy/initiate.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+### Configuration ###
+
+SERVER=luca@sfcoding.com
+APP_DIR=/var/www/dojo-site
+REMOTE_SCRIPT_PATH=/tmp/deploy-pisacoderdojo.sh
+
+### Library ###
+
+function run()
+{
+  echo "Running: $@"
+  "$@"
+}
+
+
+### Automation steps ###
+
+run scp deploy/work.sh $SERVER:$REMOTE_SCRIPT_PATH
+echo
+echo "---- Running deployment script on remote server ----"
+run ssh $SERVER sh $REMOTE_SCRIPT_PATH $APP_DIR
---
+++ deploy/work.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+### Configuration ###
+
+APP_DIR=$1
+GIT_URL=https://github.com/PisaCoderDojo/PisaCoderDojo-site.git
+RESTART_ARGS=
+
+### Automation steps ###
+TMP=/tmp/dojo-site
+
+set -x
+
+# Pull latest code
+if ! [ -e $APP_DIR ]; then
+  echo "ERROR: install wordpress first"
+fi
+
+git clone $GIT_URL $TMP
+cd $TMP
+touch $APP_DIR/wp-content/themes/test
+cp -r themes/dojo-wp-theme $APP_DIR/wp-content/themes/
+cp -r legacy_api $APP_DIR/
+rm -r $TMP
